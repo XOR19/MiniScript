625bc42c11abb8f44425e5831c7079171912e6d9
XOR19 <nils.h.emmerich@gmail.com>
2014-02-05 22:30:07 +0100

    added more javadoc

--- MiniScript/src/miniscript/MiniScriptScriptEngineFactory.java
+++ MiniScript/src/miniscript/MiniScriptScriptEngineFactory.java
@@ -18,35 +18,70 @@ public final class MiniScriptScriptEngineFactory implements ScriptEngineFactory 
 		parameters.put(ScriptEngine.NAME, MiniScriptLang.NAME);
 	}
 	
+	/**
+	 * Should only be invoked by <code>sun.misc.Service</code>
+	 */
 	public MiniScriptScriptEngineFactory(){
 		
 	}
 	
+	/**
+	 * The name of the engine
+	 * @see MiniScriptLang.ENGINE_NAME
+	 * @return {@link MiniScriptLang.ENGINE_NAME}
+	 */
 	@Override
 	public String getEngineName() {
 		return MiniScriptLang.ENGINE_NAME;
 	}
 
+	/**
+	 * The engine version
+	 * @see MiniScriptLang.ENGINE_VERSION
+	 * @return {@link MiniScriptLang.ENGINE_VERSION}
+	 */
 	@Override
 	public String getEngineVersion() {
 		return MiniScriptLang.ENGINE_VERSION;
 	}
 
+	/**
+	 * The File extension for this language
+	 * @see MiniScriptLang.EXTENSIONS
+	 * @return {@link MiniScriptLang.EXTENSIONS}
+	 */
 	@Override
 	public List<String> getExtensions() {
 		return MiniScriptLang.EXTENSIONS;
 	}
 
+	/**
+	 * The language version
+	 * @see MiniScriptLang.LANG_NAME
+	 * @return {@link MiniScriptLang.LANG_NAME}
+	 */
 	@Override
 	public String getLanguageName() {
 		return MiniScriptLang.LANG_NAME;
 	}
 
+	/**
+	 * The mime types for this language
+	 * @see MiniScriptLang.LANG_VERSION
+	 * @return {@link MiniScriptLang.LANG_VERSION}
+	 */
 	@Override
 	public String getLanguageVersion() {
 		return MiniScriptLang.LANG_VERSION;
 	}
 
+	/**
+	 * returns <code>m args0, args1, args2, ...</code>
+	 * @param obj unused
+	 * @param m assembler instruction name
+	 * @param args instruction arguments
+	 * @return a string with <code>m args0, args1, args2, ...</code>
+	 */
 	@Override
 	public String getMethodCallSyntax(String obj, String m, String... args) {
 		String ret = m;
@@ -59,26 +94,57 @@ public final class MiniScriptScriptEngineFactory implements ScriptEngineFactory 
 		return ret;
 	}
 
+	/**
+	 * The mime types for this language
+	 * @see MiniScriptLang.MIME
+	 * @return {@link MiniScriptLang.MIME}
+	 */
 	@Override
 	public List<String> getMimeTypes() {
 		return MiniScriptLang.MIME;
 	}
 
+	/**
+	 * The short names
+	 * @see MiniScriptLang.NAMES
+	 * @return {@link MiniScriptLang.NAMES}
+	 */
 	@Override
 	public List<String> getNames() {
 		return MiniScriptLang.NAMES;
 	}
 
+	/**
+	 * not used
+	 * @param toDisplay unused
+	 * @return allways <code>null</code>
+	 */
 	@Override
 	public String getOutputStatement(String toDisplay) {
 		return null;
 	}
 
+	/**
+	 * gets the parameter for a specific option
+	 * @param key the key, can be one of {{@link ScriptEngine.ENGINE}, 
+	 * {@link ScriptEngine.ENGINE_VERSION}, {@link ScriptEngine.LANGUAGE}, 
+	 * {@link ScriptEngine.LANGUAGE_VERSION}, {@link ScriptEngine.NAME}}
+	 * @return the specific parameter
+	 */
 	@Override
 	public Object getParameter(String key) {
 		return parameters.get(key);
 	}
 
+	/**
+	 * returns a program:<br>
+	 * <code>statement0<br>
+	 * statement1<br>
+	 * statement2<br>
+	 * ...</code>
+	 * @param statements some statements like <code>mov r1, 100</code>
+	 * @return a program
+	 */
 	@Override
 	public String getProgram(String... statements) {
 		String ret = "";//$NON-NLS-1$
@@ -88,6 +154,10 @@ public final class MiniScriptScriptEngineFactory implements ScriptEngineFactory 
 		return ret;
 	}
 
+	/**
+	 * creates a new scriptengine
+	 * @return a new scriptengine
+	 */
 	@Override
 	public ScriptEngine getScriptEngine() {
 		return new MiniScriptScriptEngine(this);
