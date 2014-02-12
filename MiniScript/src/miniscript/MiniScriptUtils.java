3ee8cafd7c17e4c03a2ec114c22861d292f503ff
XOR19 <nils.h.emmerich@gmail.com>
2014-02-12 14:41:10 +0100

    added java doc

--- MiniScript/src/miniscript/MiniScriptUtils.java
+++ MiniScript/src/miniscript/MiniScriptUtils.java
@@ -2,8 +2,22 @@ package miniscript;
 
 import javax.script.CompiledScript;
 
+/**
+ * 
+ * Some utils for MiniScript
+ * 
+ * @author XOR
+ *
+ */
 public final class MiniScriptUtils {
 
+	/**
+	 * 
+	 * returns a copy of the byte code compiled and executed
+	 * 
+	 * @param cs the CompiledScript
+	 * @return the copy of the byte code
+	 */
 	public static byte[] getByteCode(CompiledScript cs){
 		if(cs instanceof MiniScriptCompiledScript){
 			return ((MiniScriptCompiledScript) cs).getByteCode();
